45a0dc62a47dd6439ba22a6ad124fa166f2d9acb
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2020-02-26 18:17:52 +0900

    Dev 色々最適化

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const bodyParser = require('body-parser');
-const app = require("express")();
+const bodyParser = require('body-parser'),
+      Aquos = require("../sharp-aquos-remote-control/lib/aquos").Aquos,
+      app = require("express")(),
+      fs = require("fs"),
+      config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 app.use(bodyParser.urlencoded({ extended: true }));
-const Aquos = require("../sharp-aquos-remote-control/lib/aquos").Aquos;
-const fs = require("fs");
-const config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 
-// AQUOS接続のための設定
-// TODO: IPアドレス、ポート番号、ユーザーID、パスワードを入力する
 const control = new Aquos(
   "192.168.0.55",
   10002,
@@ -21,11 +19,47 @@ control.connect(err => {
   }
 });
 
-function checkVolume(res) {
-  control.volume(null, (err, data) => {
-    console.log("Return volume status");
-    console.log(data);
-    res.send(String(Number(data)));
+function power(arg, res) {
+  control.power(arg, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(202);
+      return;
+    }
+    res.send(Number(data) ? String(Number(data)) : String(data));
+  });
+}
+
+function volume(arg, res) {
+  control.volume(arg, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(202);
+      return;
+    }
+    res.send(Number(data) ? String(Number(data)) : String(data));
+  });
+}
+
+function channel(arg, res) {
+  control.channel(arg, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(202);
+      return;
+    }
+    res.send(Number(data) ? String(Number(data)) : String(data));
+  });
+}
+
+function input(arg, res) {
+  control.input(arg, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(202);
+      return;
+    }
+    res.send(Number(data) ? String(Number(data)) : String(data));
   });
 }
 
@@ -38,233 +72,117 @@ app.post("/", (req, res) => {
     value: req.body.value
   }
   if (request.power == "check") {
-    control.power(null, (err, data) => {
-      console.log("Return power status");
-      console.log(data);
-      res.send(String(Number(data)));
-      return;
-    });
+    return power(null, res);
   } else if (request.power == "on") {
-    control.power(true, (err, data) => {
-      console.log("Power on");
-      res.sendStatus("202");
-      return;
-    });
+    return power(true, res);
   } else if (request.power == "off") {
-    control.power(false, (err, data) => {
-      console.log("Power off");
-      res.sendStatus("202");
-      return;
-    });
+    return power(false, res);
   } else if (request.power == "toggle") {
-    control.power(null, (err, data) => {
-      console.log("Toggle power");
-      control.power(!Number(data), (err, data2) => {
-        console.log(!Number(data)?"Power on":"Power off")
-        res.send(String(Number(data)));
-        return;
-      });
-    });
+    control.power(null, (err, data) =>
+      control.power(!Number(data), (err, data2) => res.send(String(Number(data))))
+    );
+    return;
   }
 
   if (request.channel == "check") {
-    control.channel(null, (err, data) => {
-      console.log("Return channel status");
-      console.log(data);
-      res.send(String(Number(data)));
-      return;
-    });
+    return channel(null, res);
   } else if (1 <= Number(request.channel) && Number(request.channel) <= 12) {
-    control.channel(`${Number(request.channel)}1`, (err, data) => {
-      console.log("Set channel " + String(request.channel));
-      console.log(data);
-      res.send("Set channel " + String(request.channel));
-      return;
-    });
+    return channel(`${Number(request.channel)}1`, res);
   } else if (String(request.channel).match(/^\d{3}$/g)) {
-    control.channel(String(request.channel), (err, data) => {
-      console.log("Set channel " + String(request.channel));
-      res.send("Set channel " + String(request.channel));
-      return;
-    });
+    return channel(String(request.channel), res);
   } else if (request.channel == "up") {
-    control.channelUp(() => {
-      console.log("Channel up");
-      res.sendStatus(200);
-      return;
-    });
+    control.channelUp(() => res.sendStatus(200));
+    return;
   } else if (request.channel == "down") {
-    control.channelDown(() => {
-      console.log("Channel down");
-      res.sendStatus(200);
-      return;
-    });
+    control.channelDown(() => res.sendStatus(200));
+    return;
   }
 
   if (request.volume == "check") {
-    checkVolume(res);
-    return;
+    return volume(null, res);
   } else if (0 <= Number(request.volume) && Number(request.volume) <= 100) {
-    control.volume(String(request.volume), (err, data) => {
-      console.log("Set volume " + String(request.volume));
-      res.send("Set volume " + String(request.volume));
-      return;
-    });
+    return volume(String(request.volume), res);
   } else if (request.volume == "up") {
     const value = Number(request.value) ? Number(request.value) : 1;
-    control.volume(null, (err, data) => {
-      control.volume(Number(data)+value, (err, data2) => {
-        console.log("Set volume " + String(Number(data)+value));
-        res.send("Set volume " + String(Number(data)+value));
-        return;
-      })
-    });
+    control.volume(null, (err, data) =>
+      control.volume(Number(data) + value, (err, data2) => res.send(String(Number(data) + value)))
+    );
+    return;
   } else if (request.volume == "down") {
     const value = Number(request.value) ? Number(request.value) : 1;
-    control.volume(null, (err, data) => {
-      control.volume(Number(data)-value, (err, data2) => {
-        console.log("Set volume " + String(Number(data)-value));
-        res.send("Set volume " + String(Number(data)-value));
-        return;
-      })
-    });
+    control.volume(null, (err, data) =>
+      control.volume(Number(data) - value, (err, data2) => res.send(String(Number(data) - value)))
+    );
+    return;
   }
 
   if (request.input == "check") {
-    control.input(null, (err, data) => {
-      console.log("Return input status");
-      console.log(data);
-      res.send(String(Number(data)));
-      return;
-    });
+    return input(null, res);
   } else if (1 <= Number(request.input) && Number(request.input) <= 5) {
-    control.input(Number(request.input), (err, data) => {
-      console.log("Set input " + String(request.input));
-      res.send("Set input " + String(request.input));
-      return;
-    });
+    return input(Number(request.input), res);
   }
 });
 
 // 電源をつける操作のエンドポイント
 app.get("/power", (req, res) => {
   if (req.query.v == "on") {
-    control.power(true, (err, data) => {
-      console.log("Power on");
-      res.sendStatus("202");
-      return;
-    });
+    return power(true, res);
   } else if (req.query.v == "off") {
-    control.power(false, (err, data) => {
-      console.log("Power off");
-      res.sendStatus("202");
-      return;
-    });
+    return power(false, res);
   } else if (req.query.v == "toggle") {
-    control.power(null, (err, data) => {
-      console.log("Toggle power");
-      control.power(!Number(data), (err, data2) => {
-        console.log(!Number(data)?"Power on":"Power off")
-        res.send(data[0]);
-        return;
-      });
-    });
+    control.power(null, (err, data) =>
+      control.power(!Number(data), (err, data2) => res.send("status "+String(Number(data))))
+    );
+    return;
   } else {
-    control.power(null, (err, data) => {
-      console.log("Return power status");
-      console.log(data);
-      res.send(String(Number(data)));
-      return;
-    });
+    return power(null, res);
   }
 });
 
 app.get("/channel", (req, res) => {
   if (1 <= Number(req.query.v) && Number(req.query.v) <= 12) {
-    control.channel(`${Number(req.query.v)}1`, (err, data) => {
-      console.log("Set channel " + String(req.query.v));
-      console.log(data);
-      res.send("Set channel " + String(req.query.v));
-      return;
-    });
+    return channel(`${Number(req.query.v)}1`, res);
   } else if (String(req.query.v).match(/^\d{3}$/g)) {
-    control.channel(String(req.query.v), (err, data) => {
-      console.log("Set channel " + String(req.query.v));
-      res.send("Set channel " + String(req.query.v));
-      return;
-    });
+    return channel(String(req.query.v), res);
   } else if (req.query.v == "up") {
-    control.channelUp(() => {
-      console.log("Channel up");
-      res.sendStatus(200);
-      return;
-    });
+    return control.channelUp(() => res.sendStatus(200));
   } else if (req.query.v == "down") {
-    control.channelDown(() => {
-      console.log("Channel down");
-      res.sendStatus(200);
-      return;
-    });
+    return control.channelDown(() => res.sendStatus(200));
   } else {
-    control.channel(null, (err, data) => {
-      console.log("Return channel status");
-      console.log(data);
-      res.send(String(Number(data)));
-      return;
-    });
+    return channel(null, res);
   }
 });
 
 app.get("/volume", (req, res) => {
   if (0 <= Number(req.query.v) && Number(req.query.v) <= 100) {
-    control.volume(String(req.query.v), (err, data) => {
-      console.log("Set volume " + String(req.query.v));
-      res.send("Set volume " + String(req.query.v));
-      return;
-    });
+    return volume(String(req.query.v), res);
   } else if (req.query.v == "up") {
-    control.volume(null, (err, data) => {
-      control.volume(Number(data)+1, (err, data2) => {
-        console.log("Set volume " + String(Number(data)+1));
-        res.send("Set volume " + String(Number(data)+1));
-        return;
-      })
-    });
-  } else if (req.query.v == "down") {
-    control.volume(null, (err, data) => {
-      control.volume(Number(data)-1, (err, data2) => {
-        console.log("Set volume " + String(Number(data)-1));
-        res.send("Set volume " + String(Number(data)-1));
-        return;
-      })
-    });
-  } else {
-    checkVolume(res);
+    control.volume(null, (err, data) =>
+      control.volume(Number(data) + 1, (err, data2) => res.send(String(Number(data) + 1)))
+    );
     return;
+  } else if (req.query.v == "down") {
+    control.volume(null, (err, data) =>
+      control.volume(Number(data) - 1, (err, data2) => res.send(String(Number(data) + 1)))
+    );
+    return;
+  } else {
+    return volume(null, res);
   }
 });
 
 app.get("/input", (req, res) => {
   if (1 <= Number(req.query.v) && Number(req.query.v) <= 5) {
-    control.input(Number(req.query.v), (err, data) => {
-      console.log("Set input " + String(req.query.v));
-      res.send("Set input " + String(req.query.v));
-      return;
-    });
+    return input(Number(req.query.v), res);
   } else {
-    control.input(null, (err, data) => {
-      console.log("Return input status");
-      console.log(data);
-      res.send(String(Number(data)));
-      return;
-    });
+    return input(null, res);
   }
 })
 
 // サーバーの作成
 const server = app.listen(55555, () => {
-  var host = server.address().address,
-    port = server.address().port;
+  const host = server.address().address,
+        port = server.address().port;
 
   console.log("listening at http://%s:%s", host, port);
 });
